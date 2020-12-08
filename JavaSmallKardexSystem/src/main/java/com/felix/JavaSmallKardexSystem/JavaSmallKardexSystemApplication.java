bdefbefcc8bcd79487cb54e7674fecdb93a5f85c
castor-dev4 <felix.garcia@castor.com.co>
2020-12-08 14:25:08 -0500

    first commit

---
+++ JavaSmallKardexSystem/src/main/java/com/felix/JavaSmallKardexSystem/JavaSmallKardexSystemApplication.java
@@ -0,0 +1,13 @@
+package com.felix.JavaSmallKardexSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSmallKardexSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSmallKardexSystemApplication.class, args);
+	}
+
+}
---
+++ JavaSmallKardexSystem/src/test/java/com/felix/JavaSmallKardexSystem/JavaSmallKardexSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felix.JavaSmallKardexSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSmallKardexSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
