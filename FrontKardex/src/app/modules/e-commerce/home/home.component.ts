a79e750442befcf08588349e714592c44230a5d4
Felix garcia <fagal142010@gmail.com>
2020-12-11 22:14:55 -0500

    Feat: Home component

---
+++ FrontKardex/src/app/modules/e-commerce/home/home.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { StockService } from '../stock/stock.service';
+import { IProduct } from '../_models/product.model';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.scss']
+})
+export class HomeComponent {
+
+  id:number;
+  name:string;
+  quantity:number;
+  textMessage:string;
+  products:IProduct[];
+
+  constructor(private stockService: StockService) {
+    this.listProducts();
+  }
+
+  listProducts() {
+    this.textMessage="";
+    this.stockService.listProducts().subscribe((products) => {
+      this.products=products;      
+    },
+    (error) => {this.textMessage = error.error.message});
+  }
+}
